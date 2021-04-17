403ac46eee828e5b45bbf6c4ad2760c55e823448
Jatan Rathod <rathodjatan4398@gmail.com>
2021-04-17 11:39:29 +0530

    Test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./css/App.css";
 
 function App() {
-  return <div className="App">Invoice Generator App</div>;
+  return <div className="App">Invoice Generator App </div>;
 }
 
 export default App;
